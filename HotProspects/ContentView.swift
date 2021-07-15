629235b37c0850a7eb180044b75e1d58fddcaf42
VladimirMoor <81152453+VladimirMoor@users.noreply.github.com>
2021-07-15 08:57:40 +0300

    Challenge 1

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 
 struct ContentView: View {
+    
+    var prospects = Prospects()
 
     var body: some View {
         TabView {
@@ -34,6 +36,7 @@ struct ContentView: View {
                     Text("Me")
                 }
         }
+        .environmentObject(prospects)
     }
 }
 
---
+++ HotProspects/Prospects.swift
@@ -0,0 +1,51 @@
+//
+//  Prospects.swift
+//  HotProspects
+//
+//  Created by Vladimir on 13.07.2021.
+//
+
+import SwiftUI
+
+class Prospect: Identifiable, Codable {
+    let id = UUID()
+    var name = "Anonymous"
+    var emailAddress = ""
+    fileprivate(set) var isContacted = false
+}
+
+class Prospects: ObservableObject {
+    static let saveKey = "SaveData"
+    @Published private(set) var people: [Prospect]
+    
+    init() {
+        
+        if let data = UserDefaults.standard.data(forKey: Self.saveKey) {
+            if let decoded = try? JSONDecoder().decode([Prospect].self, from: data) {
+                self.people = decoded
+                return
+            }
+        }
+        
+        self.people = []
+    }
+    
+    func add(_ prospect: Prospect) {
+        people.append(prospect)
+        save()
+    }
+    
+    private func save() {
+        if let encoded = try? JSONEncoder().encode(people) {
+            UserDefaults.standard.set(encoded, forKey: Self.saveKey)
+        }
+    }
+    
+    func toggle(_ prospect: Prospect) {
+        objectWillChange.send()
+        prospect.isContacted.toggle()
+        save()
+    }
+}
+
+
